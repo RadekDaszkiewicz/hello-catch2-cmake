d6b28bb6908fb0fbb8af730d1c4a87645bcaba3e
radek <raddaszk@pg.edu.pl>
2019-01-24 10:30:35 +0100

    getAll green

--- src/db.hpp
+++ src/db.hpp
@@ -1,21 +1,14 @@
 #ifndef __MY_DB_H___
 #define __MY_DB_H___
-#include "person.hpp"
 #include "product.hpp"
 #include <list>
 #include <map>
 
 class Database {
     protected:
-    std::map<int,person_t> database;
-    std::map<int,product_t> productDB;
+    std::map<int,product_t> database;
     public:
-    std::list <person_t> getAll();
-    std::list <product_t> getAllProducts();
-    void insert(const person_t &p);
-    void insertProduct(const product_t &p);
-    void remove(const int id);
-    void removeProduct(const int id);
+    std::list <product_t> getAll();
 };
 
 #endif
--- test/db_test.cpp
+++ test/db_test.cpp
@@ -3,21 +3,13 @@
 #include <list>
 #include <map>
 #include <iostream>
+
 class DatabaseInh : public Database
 {
   public:
-    void setDb(std::map<int, person_t> d)
-    {
-        database = d;
-    };
-};
-
-class ProductDatabaseInh : public Database
-{
-public:
     void setDb(std::map<int, product_t> d)
     {
-        productDB = d;
+        database = d;
     };
 };
 
@@ -31,13 +23,13 @@ TEST_CASE("testing getting data from database", "[db][crud][getAll]")
     SECTION("getAll on empty database should return empty list")
     {
         Database db;
-        list<product_t> result = db.getAllProducts();
+        list<product_t> result = db.getAll();
         REQUIRE(result == list<product_t>{});
     }
-    SECTION("getAllProducts should return correct database")
+    SECTION("getAll should return correct database")
     {
 
-        ProductDatabaseInh db;
+        DatabaseInh db;
         std::map<int, product_t> expected = {
                 {1, {1, "Pierwszy produkt", 1.0}},
                 {2, {2, "Drugi", 2.22}}};
@@ -47,132 +39,6 @@ TEST_CASE("testing getting data from database", "[db][crud][getAll]")
         {
             expected_list.push_back(e.second);
         }
-        REQUIRE(db.getAllProducts() == expected_list);
+        REQUIRE(db.getAll() == expected_list);
     }
 }
-
-SCENARIO("database operations", "[db][crud]")
-{
-    using namespace Catch::Matchers;
-    using namespace std;
-    GIVEN("database is available with mocked data")
-    {
-        ProductDatabaseInh db;
-        map<int, product_t> alldb = {{1, {1, "Pierwszy produkt", 1.0}},
-                                    {2, {2, "Drugi", 2.22}}};
-        db.setDb(alldb);
-        list<product_t> alllist;
-        for (auto e : alldb)
-            alllist.push_back(e.second);
-        CHECK(db.getAllProducts() == alllist);
-        WHEN("one element was added to database")
-        {
-            REQUIRE_NOTHROW(db.insertProduct({3, "Kolejna rzecz", 9.99}));
-            THEN("the database should contain Kolejna rzecz ")
-            {
-                alllist.push_back({3, "Kolejna rzecz", 9.99});
-                CHECK(db.getAllProducts() == alllist);
-            }
-        }
-        WHEN("one element was removed")
-        {
-            REQUIRE_NOTHROW(db.removeProduct(1));
-            THEN("the database should contain Witkacy record")
-            {
-                alllist.pop_front();
-                CHECK(db.getAllProducts() == alllist);
-            }
-        }
-        WHEN("we try to remove nonexistent element")
-        {
-            THEN("exception should be thrown")
-            {
-                REQUIRE_THROWS_AS(db.remove(100),std::invalid_argument);
-            }
-            THEN("exception should contain correct text")
-            {
-
-                REQUIRE_THROWS_WITH(db.remove(100),
-                                    EndsWith("was not found") || StartsWith("element"));
-            }
-        }
-    }
-}
-
-//TEST_CASE("testing getting data from database", "[db][crud][getAll]")
-//{
-//    using namespace std;
-//    SECTION("we can create database object")
-//    {
-//        REQUIRE_NOTHROW([]() {Database db; return db; }());
-//    }
-//    SECTION("getAll on empty database should return empty list")
-//    {
-//        Database db;
-//        list<person_t> result = db.getAll();
-//        REQUIRE(result == list<person_t>{});
-//    }
-//    SECTION("getAll should return correct database")
-//    {
-//
-//        DatabaseInh db;
-//        std::map<int, person_t> expected = {
-//            {1, {1, "Janek"}},
-//            {2, {2, "Genowefa"}}};
-//        db.setDb(expected);
-//        list<person_t> expected_list;
-//        for (auto e : expected)
-//        {
-//            expected_list.push_back(e.second);
-//        }
-//        REQUIRE(db.getAll() == expected_list);
-//    }
-//}
-//
-//SCENARIO("database operations", "[db][crud]")
-//{
-//    using namespace Catch::Matchers;
-//    using namespace std;
-//    GIVEN("database is available with mocked data")
-//    {
-//        DatabaseInh db;
-//        map<int, person_t> alldb = {{1, {1, "Janek"}},
-//                                    {2, {2, "Genowefa"}}};
-//        db.setDb(alldb);
-//        list<person_t> alllist;
-//        for (auto e : alldb)
-//            alllist.push_back(e.second);
-//        CHECK(db.getAll() == alllist);
-//        WHEN("one element was added to database")
-//        {
-//            REQUIRE_NOTHROW(db.insert({3, "Witkacy"}));
-//            THEN("the database should contain Witkacy record")
-//            {
-//                alllist.push_back({3, "Witkacy"});
-//                CHECK(db.getAll() == alllist);
-//            }
-//        }
-//        WHEN("one element was removed")
-//        {
-//            REQUIRE_NOTHROW(db.remove(1));
-//            THEN("the database should contain Witkacy record")
-//            {
-//                alllist.pop_front();
-//                CHECK(db.getAll() == alllist);
-//            }
-//        }
-//        WHEN("we try to remove nonexistent element")
-//        {
-//            THEN("exception should be thrown")
-//            {
-//                REQUIRE_THROWS_AS(db.remove(100),std::invalid_argument);
-//            }
-//            THEN("exception should contain correct text")
-//            {
-//
-//                REQUIRE_THROWS_WITH(db.remove(100),
-//                    EndsWith("was not found") || StartsWith("element"));
-//            }
-//        }
-//    }
-//}
